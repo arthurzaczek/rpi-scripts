08552b30fa50b2dcaa1c8703dfce873fd6ee20b7
Arthur Zaczek <arthur@dasz.at>
2020-02-08 21:02:43 +0100

    initial commit of setup-backup.sh

--- README.md
+++ README.md
@@ -7,3 +7,12 @@ Source: [https://www.raspberryconnect.com/projects/65-raspberrypi-hotspot-access
 > A script to allow the Raspberry Pi to connect to a know wifi router or automatically generate a Non Internet Hotspot Access Point if no network is found. 
 
 All steps are scripted in one bashscript. Tested on raspbian buster.
+
+## setup-backup.sh
+
+Source: Unknown, commands are from var. forums, setupscript by Arthur Zaczek
+
+> A script to backup the Raspberry Pi to a NAS volume as image file
+
+All steps are scripted in one bashscript. Tested on raspbian buster.
+
---
+++ setup-backup.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+if [ "$EUID" -ne 0 ]; then 
+	echo "Please run as root"
+	exit
+fi
+
+apt-get update
+apt-get -y upgrade
+
+cp /etc/fstab ~/fstab.bak
+cat <<EOF >> /etc/fstab
+# add mount point for backup
+# TODO: change nas, user and password
+# Do not change mount point or change it alos in backup.sh
+//??nas??/home /mnt/backup cifs  username=?????,password=?????  0  0
+EOF
+
+mkdir -p /mnt/backup
+mount -a
+
+cat > /usr/local/bin/backup.sh <<EOF
+#!/bin/bash
+
+if [ "$EUID" -ne 0 ]; then 
+	echo "Please run as root"
+	exit
+fi
+
+# ------------------
+BACKUP_PATH="/mnt/backup"
+BACKUP_COUNT="5"
+BACKUP_NAME="????-Backup"
+# ------------------
+
+dd if=/dev/mmcblk0 of=${BACKUP_PATH}/${BACKUP_NAME}-$(date +%Y%m%d-%H%M%S).img bs=1MB
+pushd ${BACKUP_PATH}; ls -tr ${BACKUP_PATH}/${BACKUP_NAME}* | head -n -${BACKUP_COUNT} | xargs rm; popd
+
+EOF
+
+vim /usr/local/bin/backup.sh
+chmod a+x /usr/local/bin/backup.sh
